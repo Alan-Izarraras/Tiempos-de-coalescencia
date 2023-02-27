1058c0a11397562a94a3f9f32022bcf96a5be4ae
Alan-Izarraras <62118545+Alan-Izarraras@users.noreply.github.com>
2023-02-27 09:57:10 -0600

    bug grande corregido
    Hecho para comparar el efecto del numero de arboles en la calidad de las inferencias.

--- Comparaciones.R
+++ Comparaciones.R
@@ -1,80 +1,88 @@
+#Script para generar tabla de comparasiones de verosimilitud
+#Contiene codigo para graficar.
+#Hecho para comparar el efecto del numero de arboles en la calidad de las inferencias. 
 
+replicas <- c(1,2,3)
+valor_sel <- c("0.01", "0.1", "0", "1", "10", "100")
+runs <- c(1, 10, 50, 100)
 
-valor_ns <- c(0, 0.028, 0.28, 2.8)
-loglike_matrix <- matrix(nrow = 10, ncol=length(valor_ns))
-colnames(loglike_matrix) <- c("0Ns", "0.028Ns", "0.28Ns", "2.8Ns")
-x <- 0
+inference_matrix <- matrix(nrow=3, ncol=6)
+colnames(inference_matrix) <- c("P(0.01Ns)", "P(0.1Ns)", "P(0Ns)", "P(1Ns)", "P(10Ns)", "P(100Ns)")
 
+matriz_inferencia <- matrix(nrow=18, ncol=2)
+colnames(matriz_inferencia) <- c("loglikelihood_estimate", "seleccion")
 
-for (a in valor_ns)  {
-  x <- x + 1
-  for (n in 1:10)  {
-    Prob_rangos_ <- read.csv(paste("matriz_probabilidad_", a, "Ns_sim", n, ".csv", sep="")) #Para cambiar valor Ns tengo que cambiarlo
-    Prob_rangos_ <- Prob_rangos_[,-1]
-    Prob_rangos_ <- Prob_rangos_[,-c(8,9,10,11,12)]
-
-    matriz_conteo_rangos <- read.csv(paste("matriz_conteo_Relate.csv", sep=""))
-    matriz_conteo_rangos <- matriz_conteo_rangos[,-1]
-    matriz_conteo_rangos <- matriz_conteo_rangos[,-c(8,9,10,11,12)]
-
-    likelihoods <- vector()
-    i <- 1
-    for (r in 1:nrow(matriz_conteo_rangos))  {
-      for (c in 1:ncol(matriz_conteo_rangos))  {
-        if (matriz_conteo_rangos[r, c] != 0)  {
-          likelihoods[i] <- matriz_conteo_rangos[r,c] * log(Prob_rangos_[r, c]) #Necesito que este archivo no cambie
-          i <- i +1
+for (b in valor_sel)  {
+  for (l in runs)  {
+    p <- 1
+    for (a in valor_sel)  { 
+      for (f in replicas)  { 
+        Prob_rangos_ <- read.csv(paste("matriz_probabilidad_", a, "Ns_100runs_set2_norm_smooth_1.csv", sep="")) #Para cambiar valor Ns tengo que cambiarlo
+        Prob_rangos_ <- Prob_rangos_[,-1] #Borra la primer columna bugeada
+        matriz_conteo_rangos <- read.csv(paste("matriz_conteo_rangos_", b, "Ns_", l, "runs_replica", f, ".csv", sep="")) #10 de esto
+        matriz_conteo_rangos <- matriz_conteo_rangos[,-1]          
+        likelihoods <- vector()
+        i <- 1
+        for (r in 1:nrow(matriz_conteo_rangos))  {
+          for (c in 1:ncol(matriz_conteo_rangos))  {
+            if (matriz_conteo_rangos[r, c] != 0)  {
+              likelihoods[i] <- matriz_conteo_rangos[r,c] * log(Prob_rangos_[r, c]) #Necesito que este archivo no cambie
+              i <- i +1
+            }
+          }
         }
+
+        loglikelihood_estimate <- sum(likelihoods) 
+        inference_matrix[f, p] <- loglikelihood_estimate
+
+      } 
+      p <- p +1 
+    }
+
+    write.csv(inference_matrix, paste("inferencia_parametros_smooth_norm_v2_", b, "Ns_", l, "runs.csv", sep=""))
+  }  
+  
+}
+
+
+### Visualización ###
+
+valor_sel <- c("0.01", "0.1", "0", "1", "10", "100")
+runs <- c(1, 10, 50, 100)
+
+inferencia_Ns <- matrix(nrow=72, ncol=3)
+colnames(inferencia_Ns) <- c("loglikelihood", "seleccion", "num_corridas")
+
+for (a in valor_sel)  {
+  i <- 1
+  for (l in runs)  {
+    tabla_inferencia <- read.csv(paste("inferencia_parametros_smooth_norm_v2_", a, "Ns_", l, "runs.csv", sep=""))
+    tabla_inferencia <- tabla_inferencia[,-1]
+    colnames(tabla_inferencia) <- c("0.01Ns", "0.1Ns", "0Ns", "1Ns", "10Ns", "100Ns")
+
+    for (c in 1:ncol(tabla_inferencia))  {
+      for (r in 1:nrow(tabla_inferencia))  {
+        inferencia_Ns[i,1] <- tabla_inferencia[r,c]
+        inferencia_Ns[i,2] <- colnames(tabla_inferencia[c])
+        inferencia_Ns[i,3] <- l
+        i <- i +1
       }
     }
-    
-    loglikelihood_estimate <- sum(likelihoods) #sum(likelihoods) ## El segundo nombre cambia
-    #Ahora looglikelihood lo tengo que meter a la matriz likelihood estiamtes
-    
-    loglike_matrix[n, x] <- loglikelihood_estimate
-
-    #Matriz donde cada fila es una repeticion y cada columna un vlaor 2Ns
   }
+
+  write.csv(inferencia_Ns, paste("inferencia_", a, "Ns_.csv", sep=""))
+
 }
 
+inferencia_Ns <-read.csv("inferencia_0.1Ns.csv")
+inferencia_Ns <- inferencia_Ns[,-1]
 
-#Ahora para plottear.
+inferencia_Ns <- as.data.frame(inferencia_Ns)
+inferencia_Ns$loglikelihood <- as.numeric(inferencia_Ns$loglikelihood)
 
-colnames(loglike_matrix) <- c("0", "0.028", "0.28", "2.8")
-rownames(loglike_matrix) <- 1:10 
-
-loglike_matrix <- as.data.frame(loglike_matrix)
-
-#Lo transformo a una columna de valores log y otra columna de valores 2Ns para graficar facilmente. 
-
-loglike_matrix_plot <- matrix(nrow=40, ncol=2)
-loglike_matrix_plot[1,n]
-loglike_matrix_plot[2,n] <- colnames
-
-i <- 1
-for (c in 1:ncol(loglike_matrix))  { 
-  for (r in 1:nrow(loglike_matrix))  {   
-    z <- loglike_matrix[r, c]
-    loglike_matrix_plot[i, 2] <- colnames(loglike_matrix[c])
-    loglike_matrix_plot[i, 1] <- z
-    i <- i + 1 
-  }
-}
-
-loglike_matrix_plot <- as.data.frame(loglike_matrix_plot)
-
-#Plot. Tengo que asegurarme que datos y grupos sean valores numericos y no se transformen a char. 
-datos <- as.numeric(loglikelihood_estimate$V1)
-grupos <- as.numeric(loglikelihood_estimate$V2) 
+grafica <- ggplot(data=inferencia_Ns, mapping = aes(x=seleccion, y=loglikelihood, colour=num_corridas))
+         + geom_point(size=3, alpha=0.5) + facet_wrap(~num_corridas) + labs(title="Parametro real 100Ns")
 
 
-stripchart(datos ~ grupos, group.names = c("0Ns","0.028Ns","0.28Ns","2.8Ns"), vertical = T, pch = 20, method = "jitter", col = 2, las = 2)
-
-
-
-
-#write.csv(loglikelihood_estimates, paste("loglikelihood_estimates_50Ns.csv"))
-#jpeg("50Ns_plot", width=900, height=600)
-#boxplot(loglikelihood_estimates, main="Valor real 50Ns", xlab= "Valores 2Ns", ylab="Estimados de verosimilitud (-log)", col=4)
-#dev.off()
-
+grafica <- ggplot(data=inferencia_Ns, mapping = aes(x=seleccion, y=loglikelihood, colour=num_corridas)) + geom_point(size=3, alpha=0.5) + facet_wrap(~num_corridas) + labs(title="Parametro real 1Ns")
+ 
