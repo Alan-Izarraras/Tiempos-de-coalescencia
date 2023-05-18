671b849b3c56e894049febbb44cb4134783d8ce4
Alan-Izarraras <62118545+Alan-Izarraras@users.noreply.github.com>
2023-05-18 16:59:33 -0600

    distintas categorias
    Agregue algunas lineas para el caso donde en la matriz de maximos tengo distintas categorias (separarlas en matrices individuales)

--- RMSE_plot.R
+++ RMSE_plot.R
@@ -1,8 +1,6 @@
 
 ### RMSE ver1 usando valores 2Ns ###
 
-Necesito los valores 2 los tengo en el script de alado
-
 matriz_maximos <- read.csv("matriz_maximos_100runs.csv")
 matriz_maximos <- matriz_maximos[,-1]
 
@@ -10,20 +8,20 @@ valor_ns <- c(0,0.001, 0.003 ,0.005 ,0.01 ,0.017 ,0.03 ,0.05 ,0.1 ,0.17 ,0.31 ,0
 
 i <- 1
 v <- 1
-for (r in 1:nrow(matriz_maximos))  {
-	matriz_maximos[r,2] <- valor_ns[v]
+for (r in 1:nrow(matriz_maximos_grupo4))  {
+	matriz_maximos_grupo4[r,2] <- valor_ns[v]
 	i <- i +1
-	matriz_maximos[r,1] <- gsub("Ns", "", matriz_maximos[r,1])
+	matriz_maximos_grupo4[r,1] <- gsub("Ns", "", matriz_maximos_grupo4[r,1])
 	if (i==50)  {
 	  v <- v + 1
 	  i <- 0
 	}
 }
 
-matriz_maximos[1300,2] <- v
-matriz_maximos[,1] <- as.numeric(matriz_maximos[,1])
+#matriz_maximos_grupo2[1300,2] <- v
+matriz_maximos_grupo4[,1] <- as.numeric(matriz_maximos_grupo4[,1])
 
-sqrt(mean((matriz_maximos$parametro_real - matriz_maximos$parametro_estimado )^2))
+sqrt(mean((matriz_maximos_grupo4$parametro_real - matriz_maximos_grupo4$parametro_estimado )^2))
 
 ### RMSE plot ###
 library(ggplot)
@@ -32,8 +30,9 @@ p <- ggplot(data=RMSE, aes(x=theta, y=RMSE, color=theta, fill=theta)) +
 +     geom_bar(stat="identity")
 
 
+#Para el caso en el que tengo varias agrupaciones que quiero probar dentro de una misma matriz. 
 
-
-
-
-
+matriz_maximos_grupo1 <- matriz_maximos[which(matriz_maximos[,"agrupacion"]==1), ] 
+matriz_maximos_grupo2 <- matriz_maximos[which(matriz_maximos[,"agrupacion"]==2), ]
+matriz_maximos_grupo3 <- matriz_maximos[which(matriz_maximos[,"agrupacion"]==3), ]
+matriz_maximos_grupo4 <- matriz_maximos[which(matriz_maximos[,"agrupacion"]==4), ]   
